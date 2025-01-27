aa8a6157cb86f8185cd86e4eaae5794638f53430
AlejandroSPosada <142058005+AlejandroSPosada@users.noreply.github.com>
2025-01-27 10:40:04 -0500

    Update 047. 327. Count of Range Sum.cpp

--- leetcode/047. 327. Count of Range Sum.cpp
+++ leetcode/047. 327. Count of Range Sum.cpp
@@ -73,7 +73,7 @@ lower-pref[j] <= -pref[i-1] <= upper-pref[j]
 pref[j]-lower >= pref[i-1] >= pref[j]-upper 
 pref[j]-upper <= pref[i-1] <= pref[j]-lower
 
-EXAMPLE: (here we don't show the actually fenwick tree, but it is array normal presentation)
+EXAMPLE 1: (here we don't show the actually fenwick tree, but it is array normal presentation)
 -2,2
 nums [-2,5,-1]
 pre 0,-2,3,2
@@ -93,5 +93,41 @@ sort -2,0,2,3
 0,1,2,3,4,5,6
 0,1,1,0,1,0,0
 3
+
+EXAMPLE 2
+-2,2
+nums -2,5,-1,20,-30,10,
+pre 0,-2,3,2,22,-8,2,
+sort -8,-2,0,2,2,3,22,
+
+1) -2, 2, 3, 2
+0,1,2,3,4,5,6,
+0,0,0,1,0,0,0,0,
+1
+
+2) 3, 6, 6, 4
+0,1,2,3,4,5,6,
+0,0,1,1,0,0,0,0,
+1
+
+3) 2, 4, 6, 3
+0,1,2,3,4,5,6,
+0,0,1,1,0,0,1,0,
+3
+
+4) 22, 7, 7, 7
+0,1,2,3,4,5,6,
+0,0,1,1,1,0,1,0,
+3
+
+5) -8, 1, 1, 1
+0,1,2,3,4,5,6,
+0,0,1,1,1,0,1,1,
+3
+
+6) 2, 4, 6, 3
+0,1,2,3,4,5,6,
+0,1,1,1,1,0,1,1,
+6
 
 */
